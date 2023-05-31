eaefbde08faf4877bbd308875c45c0df6cece28d
Rose Hudson <rose@krx.sh>
2023-05-31 23:15:44 +0100

    serve both /r0 and /v3 client api
    fractal-next is still using /r0 while others are on /v3

--- src/client_api/mod.rs
+++ src/client_api/mod.rs
@@ -1,6 +1,7 @@
 use actix_web::{
     get,
     web::{self, Json},
+    Scope,
 };
 use serde_json::json;
 
@@ -12,45 +13,48 @@ mod user;
 
 pub fn configure_endpoints(cfg: &mut web::ServiceConfig) {
     cfg.service(versions);
-    let v3 = web::scope("/v3")
-        .service(auth::get_supported_login_types)
-        .service(auth::login)
-        .service(auth::logout)
-        .service(auth::logout_all)
-        .service(auth::register)
-        .service(user::get_avatar_url)
-        .service(user::set_avatar_url)
-        .service(user::get_display_name)
-        .service(user::set_display_name)
-        .service(user::get_profile)
-        .service(user::search_user_directory)
-        .service(user::get_3pids)
-        .service(room::create_room)
-        .service(room::invite)
-        .service(room::join_by_id_or_alias)
-        .service(room_events::sync)
-        .service(room_events::get_event)
-        .service(room_events::get_state_event_no_key)
-        .service(room_events::get_state_event_key)
-        .service(room_events::get_state)
-        .service(room_events::get_members)
-        .service(room_events::send_state_event)
-        .service(room_events::send_event)
-        .service(ephemeral::typing)
-        .wrap(
-            actix_cors::Cors::default()
-                .send_wildcard()
-                .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
-                .allowed_headers(vec![
-                    "Origin",
-                    "X-Requested-With",
-                    "Content-Type",
-                    "Accept",
-                    "Authorization",
-                ]),
-        );
+    let mount = |scope: Scope| {
+        scope
+            .service(auth::get_supported_login_types)
+            .service(auth::login)
+            .service(auth::logout)
+            .service(auth::logout_all)
+            .service(auth::register)
+            .service(user::get_avatar_url)
+            .service(user::set_avatar_url)
+            .service(user::get_display_name)
+            .service(user::set_display_name)
+            .service(user::get_profile)
+            .service(user::search_user_directory)
+            .service(user::get_3pids)
+            .service(room::create_room)
+            .service(room::invite)
+            .service(room::join_by_id_or_alias)
+            .service(room_events::sync)
+            .service(room_events::get_event)
+            .service(room_events::get_state_event_no_key)
+            .service(room_events::get_state_event_key)
+            .service(room_events::get_state)
+            .service(room_events::get_members)
+            .service(room_events::send_state_event)
+            .service(room_events::send_event)
+            .service(ephemeral::typing)
+            .wrap(
+                actix_cors::Cors::default()
+                    .send_wildcard()
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE", "OPTIONS"])
+                    .allowed_headers(vec![
+                        "Origin",
+                        "X-Requested-With",
+                        "Content-Type",
+                        "Accept",
+                        "Authorization",
+                    ]),
+            )
+    };
 
-    cfg.service(v3);
+    cfg.service(mount(web::scope("/r0")));
+    cfg.service(mount(web::scope("/v3")));
 }
 
 #[get("/versions")]
